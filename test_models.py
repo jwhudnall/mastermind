0b6e6ff6b6e0a386f63e0fee25f5521f21b3c4d5
James Hudnall <jimmyhudnall@gmail.com>
2022-04-10 15:07:15 -0400

    Add additional tests for Mastermind Class

--- test_models.py
+++ test_models.py
@@ -21,12 +21,38 @@ class TestHelperFunctions(TestCase):
         "winning_sequence": game.winning_sequence,
         "remaining_guess_count": game.remaining_guess_count,
         "current_guess": game.current_guess,
-        "game_over": game.game_over
+        "game_over": game.game_over,
+        "colors": 8
       })
+
+    def test_mastermind_validate_guess_inputs(self):
+      """Tests Mastermind validate_guess_inputs method."""
+      game = Mastermind()
+
+      non_int_validation = game.validate_guess_inputs(['1','invalid', '3', '4'])
+      self.assertTrue('status' in non_int_validation)
+      self.assertTrue('wasn\'t castable to a number' in non_int_validation['error'])
+
+      below_threshold_validation = game.validate_guess_inputs(['1','-3', '3', '4'])
+      self.assertTrue('status' in below_threshold_validation)
+      self.assertTrue('above or below the specified game parameters' in below_threshold_validation['error'])
+
+      above_threshold_validation = game.validate_guess_inputs(['1','12', '3', '4'])
+      self.assertTrue('status' in above_threshold_validation)
+      self.assertTrue('above or below the specified game parameters' in above_threshold_validation['error'])
+
+      above_col_threshold_validation = game.validate_guess_inputs(['1','2', '3', '4', '5'])
+      self.assertTrue('status' in above_col_threshold_validation)
+      self.assertTrue('values must equal Game' in above_col_threshold_validation['error'])
+
+      below_col_threshold_validation = game.validate_guess_inputs(['1','2', '3'])
+      self.assertTrue('status' in below_col_threshold_validation)
+      self.assertTrue('values must equal Game' in below_col_threshold_validation['error'])
+
 
     def test_mastermind_model_modified(self):
       """Tests Mastermind game class initialized as with default parameters."""
-      game = Mastermind(rows=8, cols=6)
+      game = Mastermind(rows=8, cols=6, colors=4)
       serialized = game.serialize()
 
       self.assertEqual(game.cols, 6)
@@ -41,7 +67,8 @@ class TestHelperFunctions(TestCase):
         "winning_sequence": game.winning_sequence,
         "remaining_guess_count": game.remaining_guess_count,
         "current_guess": game.current_guess,
-        "game_over": game.game_over
+        "game_over": game.game_over,
+        "colors":4
       })
 
     def test_mastermind_check_guess(self):
@@ -60,4 +87,9 @@ class TestHelperFunctions(TestCase):
         ['1','2','3','4'],
         ['2','2','1','5']
         ), {'white': 1, 'red': 1,'blank': 2
+      })
+      self.assertEqual(Mastermind.check_guess(
+        ['1','2','3','4'],
+        ['5','5','5','5']
+        ), {'white': 0, 'red': 0,'blank': 4
       })
