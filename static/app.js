61786a8addebd0bfae04f4cd98245f253c07279d
James Hudnall <jimmyhudnall@gmail.com>
2022-04-10 15:41:01 -0400

    Clean up app.js code

--- static/app.js
+++ static/app.js
@@ -1,12 +1,15 @@
-// Refactor out?
-const submitGuessBtn = $(".submitGuessBtn");
+$(document).ready(function () {
+  setListeners();
+  submitGuessBtns.click(handleGuess);
+});
+
+const submitGuessBtns = $(".submitGuessBtn");
 
 const handleGuess = async function () {
   const inputs = $("[data-dropped]");
   const values = $.map(inputs, function (val) {
     return val.dataset.dropped;
   });
-  // const JSValues = $.makeArray(values);
   const res = await sendGuessToServer(values);
   if (res) {
     const rowNum = res.game.current_guess - 1;
@@ -20,9 +23,6 @@ const handleGuess = async function () {
   }
 };
 
-// Grab all droppables to isolate current row
-// After a peg is dropped, add a data-selected attribute to the target
-
 const sendGuessToServer = async function (guessList) {
   try {
     const res = await axios.post(
@@ -30,13 +30,11 @@ const sendGuessToServer = async function (guessList) {
       { guess: { guessList } },
       { headers: { "content-type": "application/json" } }
     );
-    console.log(res.data);
 
     if (res.data.error) {
       alert("All inputs are required");
       return false;
     } else {
-      console.log("Results:");
       console.log(res.data);
       return res.data;
     }
@@ -65,22 +63,7 @@ const provideFeedback = (curRow, res) => {
   }
 };
 
-const showNextRow = (row) => {
-  $(`#row${row} td div`).each(function () {
-    $(this).addClass("droppable");
-  });
-  // Add back event listeners for droppable
-  $(".droppable").on("dragover", dragOver).off("drop", drop);
-  $(`#row-${row}-Btn`).show();
-  setListeners();
-};
-
 const disableRow = (curRow) => {
-  curRow = parseInt(curRow);
-  // Remove event listeners for droppable divs
-  $(".droppable").unbind("dragover", dragOver);
-  $(".droppable").unbind("drop", drop);
-
   $("[data-dropped]").each(function () {
     $(this).removeClass("dropped");
     $(this).removeClass("droppable");
@@ -89,19 +72,27 @@ const disableRow = (curRow) => {
   $(`#row-${curRow}-Btn`).hide();
 };
 
+const showNextRow = (row) => {
+  $(`#row${row} td div`).each(function () {
+    $(this).addClass("droppable");
+  });
+  $(`#row-${row}-Btn`).show();
+  setListeners();
+};
+
 const decrementGuessCount = () => {
   const currentVal = parseInt($("#numGuessesLeft").text());
   $("#numGuessesLeft").text(currentVal - 1);
 };
 
 const handleGameWin = (winningSequence) => {
-  submitGuessBtn.prop("hidden", true);
+  submitGuessBtns.prop("hidden", true);
   showWinningSequence(winningSequence);
   alert("Congratulations, you win!");
 };
 
 const handleGameLoss = (winningSequence) => {
-  submitGuessBtn.prop("hidden", true);
+  submitGuessBtns.prop("hidden", true);
   showWinningSequence(winningSequence);
   alert("Game Over. I'm sure you'll get it next time!");
 };
@@ -121,7 +112,6 @@ const showWinningSequence = (winningSequence) => {
   for (let i = 0; i < winningSequence.length; i++) {
     const value = winningSequence[i];
     $(`#actual-${i}`).fadeOut(function () {
-      // $(this).text(value).fadeIn(1500);
       $(this)
         .css({ "background-image": `url('../static/images/guess-choices/peg${value}.gif')` })
         .fadeIn(1500);
@@ -129,38 +119,7 @@ const showWinningSequence = (winningSequence) => {
   }
 };
 
-submitGuessBtn.click(handleGuess);
-
-// Drag Events TODO: Change to JQUERY
-// const draggableElements = document.querySelectorAll(".draggable");
-// const droppableElements = document.querySelectorAll(".droppable");
-
-// draggableElements.forEach((el) => {
-//   el.addEventListener("dragstart", dragStart);
-// });
-
-// droppableElements.forEach((el) => {
-//   el.addEventListener("dragover", dragOver);
-//   el.addEventListener("drop", drop);
-// });
-$(document).ready(function () {
-  setListeners();
-});
-
-const removeListeners = () => {
-  const draggableElements = document.querySelectorAll(".draggable");
-  const droppableElements = document.querySelectorAll(".droppable");
-
-  draggableElements.forEach((el) => {
-    el.removeEventListener("dragstart", dragStart);
-  });
-
-  droppableElements.forEach((el) => {
-    el.removeEventListener("dragover", dragOver);
-    el.removeEventListener("drop", drop);
-  });
-};
-
+// *** Drag & Drop Functionality ***
 const setListeners = () => {
   const draggableElements = document.querySelectorAll(".draggable");
   const droppableElements = document.querySelectorAll(".droppable");
