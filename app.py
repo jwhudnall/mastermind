561377704278dffed5cd15854361038874279e2c
James Hudnall <jimmyhudnall@gmail.com>
2022-04-10 20:04:19 -0400

    Update spacing, quotes

--- app.py
+++ app.py
@@ -5,51 +5,53 @@ app = Flask(__name__)
 game = Mastermind()
 
 # prevent cached responses
-if app.config["DEBUG"]:
-    @app.after_request
-    def after_request(response):
-        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-        response.headers["Expires"] = 0
-        response.headers["Pragma"] = "no-cache"
-        return response
+# if app.config['DEBUG']:
+#     @app.after_request
+#     def after_request(response):
+#         response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate, public, max-age=0"
+#         response.headers["Expires"] = 0
+#         response.headers["Pragma"] = "no-cache"
+#         return response
+
 
 @app.route('/', methods=['GET', 'POST'])
 def display_game():
-  '''
-  Primary game route. Player-defined game parameters are received as POST requests,
-  wherein the global game variable is updated.
-  '''
+    '''
+    Primary game route. Player-defined game parameters are received as POST requests,
+    wherein the global game variable is updated.
+    '''
 
-  if request.method == 'GET':
-    return render_template('index.html', game=game)
-  else:
-    global_list = globals()
-    num_cols = int(request.form.get('num_cols', None))
-    num_rows = int(request.form.get('num_rows', None))
-    num_colors = int(request.form.get('num_colors', None))
-    global_list['game'] =  Mastermind(rows=num_rows, cols=num_cols, colors=num_colors)
-    return render_template('index.html', game=game)
+    if request.method == 'GET':
+        return render_template('index.html', game=game)
+    else:
+        global_list = globals()
+        num_cols = int(request.form.get('num_cols', None))
+        num_rows = int(request.form.get('num_rows', None))
+        num_colors = int(request.form.get('num_colors', None))
+        global_list['game'] = Mastermind(
+            rows=num_rows, cols=num_cols, colors=num_colors)
+        return render_template('index.html', game=game)
+
 
 @app.route("/api/guess", methods=["POST"])
 def handle_guess():
-  '''
-  Receives and validates guess from the client.
-  '''
+    '''
+    Receives and validates guess from the client.
+    '''
 
-  guess = request.json['guess']
-  response_if_invalid = game.validate_guess_inputs(guess)
+    guess = request.json['guess']
+    response_if_invalid = game.validate_guess_inputs(guess)
 
-  if response_if_invalid:
-    return jsonify(response_if_invalid), 400
-  else:
-    results = game.check_guess(game.winning_sequence, guess)
-    game.current_guess += 1
-    game.remaining_guess_count -= 1
-    if results['red'] == game.cols:
-      game.game_over = True
+    if response_if_invalid:
+        return jsonify(response_if_invalid), 400
+    else:
+        results = game.check_guess(game.winning_sequence, guess)
+        game.current_guess += 1
+        game.remaining_guess_count -= 1
+        if results['red'] == game.cols:
+            game.game_over = True
 
-    return jsonify({
-      'results': results,
-      'game': game.serialize()
-      })
-
+        return jsonify({
+            'results': results,
+            'game': game.serialize()
+        })
--- models.py
+++ models.py
@@ -4,105 +4,106 @@
 import requests
 from flask import jsonify
 
+
 class Mastermind:
-  def __init__(self, rows=10, cols=4, colors=8):
-    '''Mastermind Game class.'''
-    self.rows = rows
-    self.cols = cols
-    self.colors = colors
-    self.winning_sequence = Mastermind.get_numbers(count=self.cols,max=self.colors - 1)
-    self.remaining_guess_count = rows
-    self.current_guess = 1
-    self.game_over = False
+    def __init__(self, rows=10, cols=4, colors=8):
+        '''Mastermind Game class.'''
+        self.rows = rows
+        self.cols = cols
+        self.colors = colors
+        self.winning_sequence = Mastermind.get_numbers(
+            count=self.cols, max=self.colors - 1)
+        self.remaining_guess_count = rows
+        self.current_guess = 1
+        self.game_over = False
 
-  def __repr__(self):
-    return f'<Mastermind rows={self.rows} cols={self.cols}>'
+    def __repr__(self):
+        return f'<Mastermind rows={self.rows} cols={self.cols}>'
 
-  def serialize(self):
-    return {
-      "rows": self.rows,
-      "cols": self.cols,
-      "winning_sequence": self.winning_sequence,
-      "remaining_guess_count": self.remaining_guess_count,
-      "current_guess": self.current_guess,
-      "colors": self.colors,
-      "game_over": self.game_over
-      }
+    def serialize(self):
+        return {
+            'rows': self.rows,
+            'cols': self.cols,
+            'winning_sequence': self.winning_sequence,
+            'remaining_guess_count': self.remaining_guess_count,
+            'current_guess': self.current_guess,
+            'colors': self.colors,
+            'game_over': self.game_over
+        }
 
-  @classmethod
-  def get_numbers(self, count, max):
-    '''
-    Retrieves random numbers from the random.org api.
-    '''
-    base_url = 'https://www.random.org/integers/'
-    params = {
-      'num': count,
-      'min':0,
-      'max':max,
-      'col':1,
-      'base':10,
-      'format': 'plain',
-      'rnd':'new'
-    }
+    @classmethod
+    def get_numbers(self, count, max):
+        '''
+        Retrieves random numbers from the random.org api.
+        '''
+        base_url = 'https://www.random.org/integers/'
+        params = {
+            'num': count,
+            'min': 0,
+            'max': max,
+            'col': 1,
+            'base': 10,
+            'format': 'plain',
+            'rnd': 'new'
+        }
 
-    res = requests.get(f'{base_url}', params=params)
-    text = res.text
-    num_list = text.split('\n')[0:-1] # list of numbers, as strings
-    return num_list
+        res = requests.get(f'{base_url}', params=params)
+        text = res.text
+        num_list = text.split('\n')[0:-1]  # list of numbers, as strings
+        return num_list
 
+    @classmethod
+    def check_guess(self, actual, guess):
+        '''Compares each value in the actual, guess lists for equality. Returns feedback'''
+        key = {
+            'white': 0,
+            'red': 0,
+            'blank': 0
+        }
+        actual_copy = actual[:]
+        guess_copy = guess[:]
 
-  @classmethod
-  def check_guess(self, actual, guess):
-    '''Compares each value in the actual, guess lists for equality. Returns feedback'''
-    key = {
-      'white': 0,
-      'red': 0,
-      'blank': 0
-    }
-    actual_copy = actual[:]
-    guess_copy = guess[:]
+        for i in range(len(guess)):
+            if guess[i] == actual[i]:
+                key['red'] = key['red'] + 1
+                # Remove for later inclusion check
+                actual_copy[i] = None
+                guess_copy[i] = None
 
-    for i in range(len(guess)):
-      if guess[i] == actual[i]:
-        key['red'] = key['red'] + 1
-        # Remove for later inclusion check
-        actual_copy[i] = None
-        guess_copy[i] = None
+        for i in range(len(guess_copy)):
+            cur_val = guess_copy[i]
+            if cur_val != None and cur_val in actual_copy:
+                key['white'] = key['white'] + 1
+                # Set index of value in actual_copy to None
+                idx = actual_copy.index(cur_val)
+                actual_copy[idx] = None
+            elif cur_val != None:
+                key['blank'] = key['blank'] + 1
 
-    for i in range(len(guess_copy)):
-      cur_val = guess_copy[i]
-      if cur_val != None and cur_val in actual_copy:
-        key['white'] = key['white'] + 1
-        # Set index of value in actual_copy to None
-        idx = actual_copy.index(cur_val)
-        actual_copy[idx] = None
-      elif cur_val != None:
-        key['blank'] = key['blank'] + 1
+        return key
 
-    return key
+    def validate_guess_inputs(self, guess):
+        '''Input validation. Checks if any values aren't castable to numbers, or are out of range Returns a JSON response with an error key indicating
+        the first failed check, if present. If all guess inputs are valid, returns False.
 
+        Accepts:
+            - guess: List of numerical strings (ex: [['1','2','3','4']])
+            - game: Mastermind Class instance.
+        Returns:
+            - False if validation passes, else JSON response with error key indicating issue.
+        '''
+        # Remove empty game slots
+        filtered = [n for n in guess if n != '']
 
-  def validate_guess_inputs(self, guess):
-    '''Input validation. Checks if any values aren't castable to numbers, or are out of range Returns a JSON response with an error key indicating
-    the first failed check, if present. If all guess inputs are valid, returns False.
+        try:
+            # Checks range (plus positive non-numerical)
+            not_in_range = any(int(val) < 0 or int(
+                val) > self.colors - 1 for val in guess)
+        except ValueError:
+            return {'error': 'One or more guess values wasn\'t castable to a number.'}
 
-    Accepts:
-        - guess: List of numerical strings (ex: [['1','2','3','4']])
-        - game: Mastermind Class instance.
-    Returns:
-        - False if validation passes, else JSON response with error key indicating issue.
-    '''
-    # Remove empty game slots
-    filtered = [n for n in guess if n != '']
-
-    try:
-      # Checks range (plus positive non-numerical)
-      not_in_range = any(int(val) < 0 or int(val) > self.colors - 1 for val in guess)
-    except ValueError:
-      return {'error':'One or more guess values wasn\'t castable to a number.'}
-
-    if len(filtered) != self.cols:
-      return {'error':'Guess values must equal Game columns.'}
-    elif not_in_range:
-      return {'error':'One or more guess values were above or below the specified game parameters.'}
-    return False
+        if len(filtered) != self.cols:
+            return {'error': 'Guess values must equal Game columns.'}
+        elif not_in_range:
+            return {'error': 'One or more guess values were above or below the specified game parameters.'}
+        return False
